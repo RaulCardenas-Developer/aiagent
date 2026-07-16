e097e849ede4eb0d6a52bd1871ef5ded4d16edf2
RaulCardenas-Developer <raulcardenas.developer@gmail.com>
2026-07-16 15:33:20 -0700

    gets file name, byte size and checks if its in directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,12 +5,28 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     
     # Will be True or False
-    
     if os.path.commonpath([working_dir_abs, target_dir]) != working_dir_abs:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
     if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
+
+
+    try:    
+        file_info = []
+        for item in os.listdir(target_dir):
+            path_file = os.path.join(target_dir, item)
+            file_size= os.path.getsize(path_file)
+            is_dir = os.path.isdir(path_file)
+            file_info.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+            
+        print(f"Result for {target_dir} directory:")
+        return "\n".join(file_info)
+
+    except Exception as e:
+        return (f"Error: Cannot list '{target_dir}' as it is outside the permitted working directory")
+
+    
     
     return f'Success: "{directory}" is within the working directory'
-        
+    
