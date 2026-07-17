53cabd745a9b7501933df42073bbe736622f2f05
RaulCardenas-Developer <raulcardenas.developer@gmail.com>
2026-07-16 19:17:26 -0700

    added run python file so ai can run the functions and tests to check if it can

---
+++ functions/run_python_file.py
@@ -0,0 +1,44 @@
+import os 
+import subprocess
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    
+    try:
+        if os.path.commonpath([working_dir_abs, target_dir]) != working_dir_abs:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+
+        if not target_dir.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", target_dir]
+        
+        if args != None:
+            command.extend(args)
+        command_process = subprocess.run(command, cwd=working_dir_abs, capture_output=True, timeout=30, text=True)
+        
+        if command_process.returncode != 0:
+            return f"Process exited with code {command_process.returncode}"
+        
+        if command_process.stdout == "" and command_process.stderr == "":
+            return "No output produced"
+
+        output_list = []
+        if command_process.stdout != "":
+            output_list.append(f"STDOUT: {command_process.stdout}")
+            
+        
+        if command_process.stderr != "":
+            output_list.append(f"STDERR: {command_process.stderr}")
+            
+        
+        result_output = "\n".join(output_list)
+        return result_output
+    except Exception as e:
+        return (f"Error: executing Python file: {e}")
